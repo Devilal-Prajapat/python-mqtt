cafcd95f52cd38ae9d7646f8efb1e925647c94e5
Devilal Prajapat <13392395+Devilal-Prajapat@users.noreply.github.com>
2024-09-06 12:18:32 +0530

    Update pub_sub.py

--- pub_sub.py
+++ pub_sub.py
@@ -3,63 +3,112 @@ import ssl
 from time import sleep
 from datetime import datetime as dt
 import json
+import random
+
+client = None
+PUB_TOPIC = "sbos/demo/pub"
+SUB_TOPIC = "sbos/demo/sub"
+
+# MQTT_HOST = "broker.hivemq.com"
+MQTT_HOST = "test.mosquitto.org"
+MQTT_PORT = 1883
 
 
-pub_topic = "rpi/pub"
-sub_topic = "rpi/sub"
-mqtt_host = ""
-mqtt_port = 8883
+def PublishToTopic(pubtopic:str,msg,qos=1):
+    global client
+    try:
+        json_msg = msg #json.loads(msg)
+        client.publish(pubtopic,json_msg, qos = qos)  
+    except Exception as e:
+        print(f"PublishToTopic exception:{e}")
+
+def SubscribeToTopic(topic, qos = 0):
+    global client
+    try:
+        client.subscribe(topic=topic, qos = qos)
+    except Exception as e:
+        print(f"SubscribeToTopic exception:{e}")
 
 
+# This callback are now in application code 
 def is_json_key_present(json, key):
     try:
         buf = json[key]
-    except KeyError:
+    except Exception as e:
+        print("Key Error {e}")
         return False
     return True
 
 def handle_incomming_msg(msg):
-    json_msg = json.loads(msg)
+    try:
+        json_msg = json.loads(msg)
+    except Exception as e:
+        print(f"Json Decode Error {e}")
+        return
 
     if is_json_key_present(json_msg, "led"):
         print("led state {}".format(json_msg["led"]))
-
     
-def on_connect(client, userdata, flags, reason_code, properties=None):
+def on_connect_cb(client, userdata, flags, reason_code, properties=None):
     print(f"Connected with result code {reason_code}")
-    client.subscribe(topic=pub_topic)
+    SubscribeToTopic(topic=SUB_TOPIC,qos = 0)
 
-def on_disconnect(client, userdata, rc):
+def on_disconnect_cb(client, userdata, rc):
     print("disconect call back")
+    MQTTConnect(MQTT_HOST,MQTT_PORT)
 
-def on_message(client, userdata, message, properties=None):
+def on_message_cb(client, userdata, message, properties=None):
     handle_incomming_msg(message.payload)
     print(
         f"{dt.now()} Received message {message.payload} on topic '{message.topic}' with QoS {message.qos}"
     )
     #handle_incomming_msg(message.payload)
 
-def on_subscribe(client, userdata, mid, qos, properties=None):
+def on_subscribe_cb(client, userdata, mid, qos, properties=None):
     print(f"{dt.now()} Subscribed with QoS {qos}")
 
-client = mqtt.Client(client_id="clientid",callback_api_version=mqtt.CallbackAPIVersion.VERSION1, clean_session=True)
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
-client.on_message = on_message
-client.on_subscribe = on_subscribe
-#tls_set(ca_certs: str | None = None, certfile: str | None = None, keyfile: str | None = None, cert_reqs: VerifyMode | None = None, tls_version: int | None = None, ciphers: str | None = None, keyfile_password: str | None = None, alpn_protocols: list[str] | None = None) → 
-client.tls_set(ca_certs="certs/aws_root_ca.pem",
-               certfile ="certs/client.crt",
-               keyfile="certs/client.key")
-client.connect(host=mqtt_host, port=mqtt_port, keepalive=60)
-client.loop_start()
-count = 0
-state = True
-while True:
-    count += 1
-    msg = {"temp": 50, "count": count,"led":state }
-    state = not(state)
-    json_msg = json.dumps(msg)
-    client.publish(pub_topic,json_msg,qos = 1)  
-    sleep(1)
-client.loop_stop()
+
+def MQTTInit(on_connect_cb, on_disconnect_cb, on_message_cb, on_subscribe_cb, USE_SSL= 0):
+    global client
+    clientId = f"IOT-Thing-{str(random.randint(1, 1000000))}"
+    print(clientId)
+    client = mqtt.Client(client_id=clientId, clean_session=True)
+    '''
+    client.on_connect = on_connect_cb
+    client.on_disconnect = on_disconnect_cb
+    client.on_message = on_message_cb
+    client.on_subscribe = on_subscribe_cb 
+    '''
+
+    client.on_connect = on_connect_cb
+    client.on_disconnect = on_disconnect_cb
+    client.on_message = on_message_cb
+    client.on_subscribe = on_subscribe_cb
+    #tls_set(ca_certs: str | None = None, certfile: str | None = None, keyfile: str | None = None, cert_reqs: VerifyMode | None = None, tls_version: int | None = None, ciphers: str | None = None, keyfile_password: str | None = None, alpn_protocols: list[str] | None = None) → 
+    if USE_SSL:
+        client.tls_set(ca_certs="certs/aws_root_ca.pem",
+                certfile ="certs/client.crt",
+                keyfile="certs/private.key")
+
+def MQTTConnect(mqtt_host, mqtt_port):
+    global client
+    client.connect(host=mqtt_host, port=mqtt_port, keepalive=60)
+    client.loop_start()
+
+def MQTTDisconnect():
+     global client
+     client.loop_stop()
+
+if __name__ == "__main__":
+    MQTTInit(on_connect_cb, on_disconnect_cb, on_message_cb, on_subscribe_cb,USE_SSL=False)
+    MQTTConnect(MQTT_HOST, MQTT_PORT)
+    count = 0
+    state = True
+    while True:
+        count += 1
+        msg = {"temp": 50, "count": count,"led":state }
+        state = not(state)
+        json_msg = json.dumps(msg)
+        PublishToTopic(PUB_TOPIC,json_msg,qos = 1)  
+        sleep(10)
+    MQTTDisconnect()
